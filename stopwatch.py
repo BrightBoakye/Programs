4849f160c1e1cba470e5cbc167c552c4bf08838c
TheBase99 <87952798+TheBase99@users.noreply.github.com>
2022-08-03 02:12:00 +0000

    Add files via upload

---
+++ stopwatch.py
@@ -0,0 +1,23 @@
+import time
+print('''StopWatch Made by Dankwah Boakye Bright.
+@Briytt.t on Instagram; @TBEmusk on Twitter ''')
+
+print('''Press ENTER to begin. Afterward, press ENTER to "click" the stopwatch.\n
+      Press Ctrl-C to quit''')
+
+input()
+print('Started')
+startTime = time.time()
+lastTime = startTime
+lapNum = 1
+
+try:
+    while True:
+        input()
+        lapTime = round(time.time()-lastTime, 2)
+        totalTime = round(time.time() - startTime, 2)
+        print('Lap #%s: %s (%s)' % (lapNum, totalTime, lapTime), end = '')
+        lapNum += 1
+        lastTime = time.time()
+except KeyboardInterrupt:
+    print('\nDone.')
